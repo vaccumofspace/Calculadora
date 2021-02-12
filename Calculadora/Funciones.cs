2fee932026cabb85c3b561bcdde8cfdc60b9258c
manuel41 <medmundogonzalez@gmail.com>
2021-02-12 17:20:04 -0400

    Crear funcion de Sumar dos numeros

---
+++ Calculadora/Funciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public static class Funciones
+    {
+        public static int Sumar(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+    }
+}
